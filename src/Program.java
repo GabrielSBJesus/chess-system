61d91f8f8961045f8dd50d875f5fd7a41f248758
Gabriel <gabrielsantosbomjesus@gmail.com>
2022-11-28 02:09:00 -0300

    first class: Position

--- src/Program.java
+++ src/Program.java
@@ -3,6 +3,6 @@ import boardgame.Position;
 public class Program {
     public static void main(String[] args) {
         Position position = new Position(3, 5);
-        System.out.println(position);
+        System.out.print(position);
     }
 }
